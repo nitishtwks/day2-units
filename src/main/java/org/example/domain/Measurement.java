f6411446c7c08b2d47e184b0a2a13fe4b7f33894
Ramcharan.Teja <ramcharan.teja@thoughtworks.com>
2023-11-07 12:37:14 +0530

    [Nitish/Ramcharan] Add type conversions

--- src/main/java/org/example/domain/Measurement.java
+++ src/main/java/org/example/domain/Measurement.java
@@ -2,22 +2,57 @@ package org.example.domain;
 
 public class Measurement {
 
-    private final int value;
+    private final float value;
 
-    private final String unit;
+    private final Unit unit;
 
-    public Measurement(int value, String unit) throws IllegalArgumentException {
+    private float normalizedValue;
+    
+    public Measurement(float value, Unit unit) throws IllegalArgumentException {
         if(unit==null){
             throw new IllegalArgumentException("Please provide a valid unit");
         }
         this.value = value;
         this.unit = unit;
+        this.covertIntoDefault(value,unit);
     }
 
-    public boolean isEqualTo(Measurement measurement){
-        if(measurement.value==this.value && measurement.unit.equals(this.unit)){
-            return true;
+    private void covertIntoDefault(float value, Unit unit) {
+        switch (unit) {
+            case KM:
+                normalizedValue = 1000*value;
+                break;
+            case M:
+                normalizedValue = 100*value;
+                break;
+            default:
+                normalizedValue = value;
+                break;
         }
-        return false;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Float.floatToIntBits(value);
+        result = prime * result + ((unit == null) ? 0 : unit.hashCode());
+        result = prime * result + Float.floatToIntBits(normalizedValue);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Measurement other = (Measurement) obj;
+        if (Float.floatToIntBits(normalizedValue) != Float.floatToIntBits(other.normalizedValue))
+            return false;
+        return true;
+    }
+    
 }
--- src/test/java/org/example/test/MeasurementTest.java
+++ src/test/java/org/example/test/MeasurementTest.java
@@ -1,6 +1,7 @@
 package org.example.test;
 
 import org.example.domain.Measurement;
+import org.example.domain.Unit;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,20 +10,34 @@ class MeasurementTest {
 
     @Test
     void shouldEqualToMeasurement() throws IllegalAccessException {
-        Measurement m1 = new Measurement(1, "cm");
-        Measurement m2 = new Measurement(1, "cm");
-        assertTrue(m1.isEqualTo(m2));
+        Measurement m1 = new Measurement(1, Unit.CM);
+        Measurement m2 = new Measurement(1, Unit.CM);
+        assertTrue(m1.equals(m2));
     }
 
     @Test
     void shouldNotEqualToMeasurement() throws IllegalAccessException {
-        Measurement m1 = new Measurement(1, "cm");
-        Measurement m2 = new Measurement(2, "cm");
-        assertFalse(m1.isEqualTo(m2));
+        Measurement m1 = new Measurement(1, Unit.CM);
+        Measurement m2 = new Measurement(2, Unit.CM);
+        assertFalse(m1.equals(m2));
     }
 
     @Test
     void shouldThrowExceptionWhenInvalidUnitProvided() throws IllegalAccessException {
         assertThrows(IllegalArgumentException.class, () ->  new Measurement(1, null));
     }
+
+    @Test
+    void shouldMeasurementInMEqualToCM() throws IllegalAccessException {
+        Measurement m1 = new Measurement(1, Unit.M);
+        Measurement m2 = new Measurement(100, Unit.CM);
+        assertTrue(m1.equals(m2));
+    }
+    @Test
+    void shouldMeasurementInCMEqualToKM() throws IllegalAccessException {
+        Measurement m1 = new Measurement(100, Unit.CM);
+        Measurement m2 = new Measurement(0.0001F, Unit.KM);
+        assertFalse(m1.equals(m2));
+    }
+
 }
