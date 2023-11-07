65f148b6e08db036cf538e9425199e185ad47897
Nitish Deshpande <nitish.deshpande@Nitishs-MBP.bbrouter>
2023-11-07 13:09:49 +0530

    Refactor test case to remove unused code

--- src/test/java/org/example/test/MeasurementTest.java
+++ src/test/java/org/example/test/MeasurementTest.java
@@ -9,32 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class MeasurementTest {
 
     @Test
-    void shouldEqualToMeasurement() throws IllegalAccessException {
+    void shouldEqualToMeasurement() {
         Measurement m1 = new Measurement(1, Unit.CM);
         Measurement m2 = new Measurement(1, Unit.CM);
         assertTrue(m1.equals(m2));
     }
 
     @Test
-    void shouldNotEqualToMeasurement() throws IllegalAccessException {
+    void shouldNotEqualToMeasurement() {
         Measurement m1 = new Measurement(1, Unit.CM);
         Measurement m2 = new Measurement(2, Unit.CM);
         assertFalse(m1.equals(m2));
     }
 
     @Test
-    void shouldThrowExceptionWhenInvalidUnitProvided() throws IllegalAccessException {
+    void shouldThrowExceptionWhenInvalidUnitProvided() {
         assertThrows(IllegalArgumentException.class, () ->  new Measurement(1, null));
     }
 
     @Test
-    void shouldMeasurementInMEqualToCM() throws IllegalAccessException {
+    void shouldMeasurementInMEqualToCM() {
         Measurement m1 = new Measurement(1, Unit.M);
         Measurement m2 = new Measurement(100, Unit.CM);
         assertTrue(m1.equals(m2));
     }
     @Test
-    void shouldMeasurementInCMEqualToKM() throws IllegalAccessException {
+    void shouldMeasurementInCMEqualToKM() {
         Measurement m1 = new Measurement(100, Unit.CM);
         Measurement m2 = new Measurement(0.0001F, Unit.KM);
         assertFalse(m1.equals(m2));
