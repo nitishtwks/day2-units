1afe60e079e0fbadf0aa2c60d833c1c1a6ded3ad
Nitish Deshpande <nitish.deshpande@thoughtworks.com>
2023-11-07 17:23:22 +0530

    Update test cases with inputs in requirement

--- src/test/java/org/example/test/temperature/TemperatureTest.java
+++ src/test/java/org/example/test/temperature/TemperatureTest.java
@@ -9,35 +9,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class TemperatureTest {
 
     @Test
-    void shouldEqualToMeasurement() {
+    void shouldEqualToMeasurementCelsiusToFar() {
         Temperature m1 = new Temperature(1, TemperatureUnit.C);
         Temperature m2 = new Temperature(33.8f, TemperatureUnit.F);
         assertTrue(m1.equals(m2));
     }
 
     @Test
-    void shouldNotEqualToMeasurement() {
-        Temperature m1 = new Temperature(1, TemperatureUnit.C);
-        Temperature m2 = new Temperature(33.8f, TemperatureUnit.F);
+    void shouldNotEqualToMeasurementCelsiusToFar() {
+        Temperature m1 = new Temperature(0, TemperatureUnit.C);
+        Temperature m2 = new Temperature(1, TemperatureUnit.F);
         assertFalse(m1.equals(m2));
+    }
+
+    @Test
+    void shouldEqualToMeasurementCelsiusToKelvin() {
+        Temperature m1 = new Temperature(0, TemperatureUnit.C);
+        Temperature m2 = new Temperature(273.15f, TemperatureUnit.K);
+        assertTrue(m1.equals(m2));
     }
 
     @Test
     void shouldThrowExceptionWhenInvalidUnitProvided() {
         assertThrows(IllegalArgumentException.class, () ->  new Temperature(1, null));
     }
-
-    @Test
-    void shouldMeasurementInMEqualToCM() {
-        Temperature m1 = new Temperature(1, TemperatureUnit.C);
-        Temperature m2 = new Temperature(100, TemperatureUnit.C);
-        assertTrue(m1.equals(m2));
-    }
-    @Test
-    void shouldMeasurementInCMEqualToKM() {
-        Temperature m1 = new Temperature(100, TemperatureUnit.C);
-        Temperature m2 = new Temperature(0.0001F, TemperatureUnit.C);
-        assertFalse(m1.equals(m2));
-    }
-
 }
